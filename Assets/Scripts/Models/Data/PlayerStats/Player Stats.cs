731d07bf25d447bd2f76c884e7e5371a65718b2a
Jackboodles <Jackinjas@gmail.com>
2025-10-16 19:08:56 +0300

    PlayerStatsScript

---
+++ Assets/Scripts/Models/Data/PlayerStats/Player Stats.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class NewMonoBehaviourScript : ScriptableObject
+{
+
+    //Assumed Stats for player to have ( Damn it bluebeard I DUNNO WHAT WE ARE DOING HERE )
+
+    [Header("Progression")]
+    public int StageLevel = 0;
+    public int CollectedPoints = 0;
+
+    [Header("Health Tracker")]
+    public int MaxHealth = 5;
+    public int CurrentHealth = 5;
+
+    [Header("Ability Tracker")]
+    public int MaxAbilityCharges = 0;
+    public int CurrentAbilityCharges = 0;
+
+    [Header("Movement")]
+    public float JumpHeight = 5f;
+    public float FallSpeed = 1f;
+    public float HorizontalSpeed = 5f;
+    
+    [Header("Time Tracker")]
+    public float TimeElapsed = 0f;
+
+    //Upon fulfilling certain conditions from which Stats need to be reset this'll be the result.
+    public void ResetStats()
+    {
+        CollectedPoints = 0;
+        CurrentHealth = MaxHealth;
+        CurrentAbilityCharges = MaxAbilityCharges;
+        TimeElapsed = 0f;
+
+    }
+
+
+
+    //
+}
