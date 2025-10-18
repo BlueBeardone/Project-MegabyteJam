4caef46e0fc1288ea3091f0396597848e481bc12
BlueBeardone <barend.army@gmail.com>
2025-10-18 14:35:55 +0200

    Added some skills

---
+++ Assets/Scripts/Gameplay/Skills/MovementSkills/DoubleJumpSkill.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class DoubleJumpSkill : ISkill
+{
+    public SkillType Type => SkillType.DoubleJump;
+
+    public float Cooldown => 2.0f;
+
+    public string Description => "Allows the player to jump a second time while in the air.";
+
+    public void Execute(GameObject player)
+    {
+        var movement = player.GetComponent<PlayerMovement>(); // Waiting for Jack's PlayerMovement script
+        if (movement != null && movement.CanDoubleJump)
+        {
+            movement.Jump();
+            movement.CanDoubleJump = false;
+        }
+    }
+}
