40d48c661146865dc55b4bf5bce96637da74c887
Jackboodles <Jackinjas@gmail.com>
2025-10-18 16:47:06 +0300

    Player Movement Update

--- Assets/Scripts/Gameplay/Movement/PlayerMovement.cs
+++ Assets/Scripts/Gameplay/Movement/PlayerMovement.cs
@@ -1,15 +1,18 @@
+using UnityEditor;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 
-  
-   
+
+
 {
     public Rigidbody2D body;
 
+ 
+
     [SerializeField] PlayerStats stats;
 
-   
+
 
     // Update is called once per frame
     void Update()
@@ -17,10 +20,27 @@ public class PlayerMovement : MonoBehaviour
         float xInput = Input.GetAxis("Horizontal");
         float yInput = Input.GetAxis("Vertical");
 
+
+        // For more fluid movement
+
+        if (Mathf.Abs(xInput) > 0)
+        {
+            body.linearVelocity = new Vector2(xInput * stats.Speed, body.linearVelocity.y);
+        }
+
+        if (Mathf.Abs(yInput) > 0)
+        {
+            body.linearVelocity = new Vector2(body.linearVelocity.x, yInput * stats.Speed);
+        }
+
+
         Vector2 direction = new Vector2(xInput, yInput).normalized;
         body.linearVelocity = direction * stats.Speed;
-        
+
+
+
     }
 
     
-}
+    }
+
