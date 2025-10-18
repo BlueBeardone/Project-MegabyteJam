755c43532012e5bcd4951722aa3d36c7b9bace8b
BlueBeardone <barend.army@gmail.com>
2025-10-18 14:29:20 +0200

    Initial setup

---
+++ Assets/Scripts/Gameplay/Skills/ISkill.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public interface ISkill
+{
+    SkillType Type { get; }
+    void Execute(GameObject player);
+    float Cooldown { get; }
+    string Description { get; }
+}
+
---
+++ Assets/Scripts/Models/Enums/Skilltype.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+
+public enum SkillType
+{
+    DoubleJump,
+    Dash,
+    GroundSlam
+}
+
